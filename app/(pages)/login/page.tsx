052a4376ac2e1d29076a5f58fd9fefd4d0e9b094
redig <luiz@redig@gmail.com>
2024-07-01 16:03:00 -0300

    style: update login page layout

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -77,19 +77,19 @@ const Page = () => {
     <>
       <div className="flex h-screen w-full flex-col sm:flex-row">
         {/* Left */}
-        <div className="flex h-full w-full flex-col items-center justify-center gap-12 sm:w-1/2">
+        <div className="flex h-full w-full flex-col items-center justify-center gap-12 md:w-1/2">
           {/* Title */}
-          <p className="sm:text-2xl md:text-3xl">
+          <p className="md:text-3xl">
             Bem-vindo(a) ao <span className="font-semibold">RAO</span>
           </p>
 
-          <div className="relative flex h-1/2 w-1/2">
+          <div className="relative flex h-1/2 w-1/2 items-center justify-center">
             {/* Green circle */}
             <div className="absolute left-[-50px] top-[-30px] h-32 w-32 rounded-full bg-green-400"></div>
 
             {/* Form */}
-            <Card className="absolute z-[1] flex h-full w-full items-center justify-center bg-white/20 shadow-lg ring-1 ring-black/5 backdrop-blur-xl">
-              <CardContent className="p-6">
+            <Card className="absolute z-[1] flex h-full w-full min-w-72 items-center justify-center bg-white/20 shadow-lg backdrop-blur-xl">
+              <CardContent className="p-5">
                 <div className="flex w-full items-center justify-center">
                   <Form {...form}>
                     <form
@@ -152,7 +152,7 @@ const Page = () => {
         </div>
 
         {/* Right */}
-        <div className="hidden items-center justify-center bg-primary sm:flex sm:w-1/2">
+        <div className="hidden items-center justify-center bg-primary sm:w-1/2 md:flex">
           <Image
             src={"/undraw_learning_re_32qv.svg"}
             alt="Image"
